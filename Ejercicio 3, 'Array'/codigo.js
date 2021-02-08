bdbff4c35683ad2a0da2d70105f649f466abc199
Alba2609 <77722805+Alba2609@users.noreply.github.com>
2021-02-08 11:40:28 +0100

    Add files via upload

---
+++ Ejercicio 3, 'Array'/codigo.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var miArray = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+];
+
+console.log(document.write(miArray));
